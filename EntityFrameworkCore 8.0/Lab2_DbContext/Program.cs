015bc289182ce782411a756195a0991bf318e5e7
Sanjana_Kumbhar <kumbharsanjana97@gmail.com>
2026-07-07 12:12:57 +0530

    Add Program.cs for Retail Inventory System

---
+++ EntityFrameworkCore 8.0/Lab2_DbContext/Program.cs
@@ -0,0 +1,7 @@
+using Lab2_DbContext.Data;
+
+Console.WriteLine("Retail Inventory System");
+
+using var context = new AppDbContext();
+
+Console.WriteLine("Database Context Created Successfully.");
