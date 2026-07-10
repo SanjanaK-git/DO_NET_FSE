cbc792f5b27aae06a2823d01879bc7c4604636c0
Sanjana_Kumbhar <kumbharsanjana97@gmail.com>
2026-07-10 12:57:49 +0530

    Add Product class for database model

---
+++ EntityFrameworkCore 8.0/Lab5-Retrieving Data from the Database/Product.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Lab5_RetrieveData.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+    }
+}
