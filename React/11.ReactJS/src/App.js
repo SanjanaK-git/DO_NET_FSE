7f213d80c5cf9b4ffca37a59d20293dfcab86487
Sanjana_Kumbhar <kumbharsanjana97@gmail.com>
2026-07-23 20:55:20 +0530

    Create App.js

---
+++ React/11.ReactJS/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import EventExample from "./EventExample";
+import CurrencyConvertor from "./Components/CurrencyConvertor";
+
+function App() {
+
+  return (
+    <div>
+
+      <EventExample />
+
+      <hr />
+
+      <CurrencyConvertor />
+
+    </div>
+  );
+}
+
+export default App;
