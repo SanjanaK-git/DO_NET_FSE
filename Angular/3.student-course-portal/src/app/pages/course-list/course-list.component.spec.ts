489b107e2de3f74945ea8d4b5f11be19dbaaa496
Sanjana_Kumbhar <kumbharsanjana97@gmail.com>
2026-07-24 00:36:43 +0530

    Add unit tests for CourseListComponent

---
+++ Angular/3.student-course-portal/src/app/pages/course-list/course-list.component.spec.ts
@@ -0,0 +1,36 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CourseListComponent } from './course-list.component';
+
+describe('CourseListComponent', () => {
+
+  let component: CourseListComponent;
+  let fixture: ComponentFixture<CourseListComponent>;
+
+  beforeEach(async () => {
+
+    await TestBed.configureTestingModule({
+      imports: [CourseListComponent]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(CourseListComponent);
+    component = fixture.componentInstance;
+
+    fixture.detectChanges();
+
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should contain courses', () => {
+    expect(component.courses.length).toBe(3);
+  });
+
+  it('should update selected course id', () => {
+    component.onEnroll(2);
+    expect(component.selectedCourseId).toBe(2);
+  });
+
+});
