c4eece27785680378a66e6404482dbcdc01a357e
Sanjana_Kumbhar <kumbharsanjana97@gmail.com>
2026-07-23 19:35:20 +0530

    Create Home.js

---
+++ React/5.ReactJS/src/Components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h2>Welcome to Cognizant Academy</h2>
+      <p>Trainer Management Portal</p>
+    </div>
+  );
+}
+
+export default Home;
