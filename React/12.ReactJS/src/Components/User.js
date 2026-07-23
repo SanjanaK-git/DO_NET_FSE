ba30ec656ee6f779f54af5e4231b1087074423c7
Sanjana_Kumbhar <kumbharsanjana97@gmail.com>
2026-07-23 21:03:57 +0530

    Add User component for flight ticket booking

---
+++ React/12.ReactJS/src/Components/User.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function User() {
+  return (
+    <div>
+      <h2>Welcome Logged In User</h2>
+
+      <h3>Book Your Flight Ticket</h3>
+
+      <button>
+        Book Ticket
+      </button>
+    </div>
+  );
+}
+
+export default User;
