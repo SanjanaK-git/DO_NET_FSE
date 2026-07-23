46840cf0396d6f32f5da67b53f842e817836608b
Sanjana_Kumbhar <kumbharsanjana97@gmail.com>
2026-07-23 22:07:41 +0530

    Create App component with Register import

---
+++ React/16.ReactJS/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Register from "./Components/Register";
+
+
+function App(){
+
+  return(
+
+    <div>
+
+      <Register />
+
+    </div>
+
+  );
+
+}
+
+
+export default App;
