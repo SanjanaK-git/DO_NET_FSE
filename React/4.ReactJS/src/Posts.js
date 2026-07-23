a454acacd6a573903f0780afa758f2b4631cc337
Sanjana_Kumbhar <kumbharsanjana97@gmail.com>
2026-07-23 19:10:50 +0530

    Add Posts component to fetch and display blog posts

---
+++ React/4.ReactJS/src/Posts.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+
+class Posts extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            posts: []
+        };
+    }
+
+    loadPosts() {
+        fetch("https://jsonplaceholder.typicode.com/posts")
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    posts: data
+                });
+            })
+            .catch(error => {
+                alert(error);
+            });
+    }
+
+    componentDidMount() {
+        this.loadPosts();
+    }
+
+    componentDidCatch(error) {
+        alert("Error: " + error);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Blog Posts</h1>
+
+                {this.state.posts.map(post => (
+                    <div key={post.id}>
+                        <h2>{post.title}</h2>
+                        <p>{post.body}</p>
+                        <hr />
+                    </div>
+                ))}
+            </div>
+        );
+    }
+}
+
+export default Posts;
