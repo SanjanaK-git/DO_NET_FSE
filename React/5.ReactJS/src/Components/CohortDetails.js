5a85796ec4dbc562d60a57c3ba384fcf694dc7fc
Sanjana_Kumbhar <kumbharsanjana97@gmail.com>
2026-07-23 19:25:13 +0530

    Refactor CohortDetails component with CSS module

--- React/5.ReactJS/src/Components/CohortDetails.js
+++ React/5.ReactJS/src/Components/CohortDetails.js
@@ -1,12 +1,33 @@
-.box {
-    width: 300px;
-    display: inline-block;
-    margin: 10px;
-    padding: 10px 20px;
-    border: 1px solid black;
-    border-radius: 10px;
+import React from "react";
+import styles from "../CohortDetails.module.css";
+
+function CohortDetails(props) {
+
+    return (
+        <div className={styles.box}>
+
+            <h3 style={{
+                color: props.status === "ongoing" ? "green" : "blue"
+            }}>
+                {props.name}
+            </h3>
+
+            <dl>
+                <dt>Cohort Name:</dt>
+                <dd>{props.name}</dd>
+
+                <dt>Status:</dt>
+                <dd>{props.status}</dd>
+
+                <dt>Start Date:</dt>
+                <dd>{props.startDate}</dd>
+
+                <dt>End Date:</dt>
+                <dd>{props.endDate}</dd>
+            </dl>
+
+        </div>
+    );
 }
 
-dt {
-    font-weight: 500;
-}
+export default CohortDetails;
