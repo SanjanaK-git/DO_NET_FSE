05e47dafec1b3f23cd64cd4b172bd97e689c1c98
Sanjana_Kumbhar <kumbharsanjana97@gmail.com>
2026-07-23 22:25:41 +0530

    Initialize App component with Getuser import

---
+++ React/17.ReactJS/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Getuser from "./Components/Getuser";
+
+
+function App(){
+
+
+  return (
+
+    <div>
+
+      <Getuser />
+
+    </div>
+
+  );
+
+}
+
+
+export default App;
