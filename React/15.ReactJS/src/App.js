3c86945926dfb9ad98bd97bc083dcb870baf9221
Sanjana_Kumbhar <kumbharsanjana97@gmail.com>
2026-07-23 21:58:35 +0530

    Create App component to render ComplaintRegister

---
+++ React/15.ReactJS/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ComplaintRegister from "./Components/ComplaintRegister";
+
+
+function App(){
+
+  return(
+
+    <div>
+
+      <ComplaintRegister />
+
+    </div>
+
+  );
+
+}
+
+
+export default App;
