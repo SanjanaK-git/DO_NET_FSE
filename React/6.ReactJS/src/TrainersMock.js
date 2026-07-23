d2c9e5262a679959be1488c89e941c7ee4ac523e
Sanjana_Kumbhar <kumbharsanjana97@gmail.com>
2026-07-23 19:41:00 +0530

    Add TrainersMock.js with sample trainer data

---
+++ React/6.ReactJS/src/TrainersMock.js
@@ -0,0 +1,30 @@
+import Trainer from "./Trainer";
+
+const trainers = [
+  new Trainer(
+    1,
+    "John",
+    "john@gmail.com",
+    "9876543210",
+    "React",
+    "React, JavaScript, HTML, CSS"
+  ),
+  new Trainer(
+    2,
+    "Alice",
+    "alice@gmail.com",
+    "9876543211",
+    "Java",
+    "Java, Spring Boot"
+  ),
+  new Trainer(
+    3,
+    "David",
+    "david@gmail.com",
+    "9876543212",
+    ".NET",
+    "C#, ASP.NET Core"
+  )
+];
+
+export default trainers;
