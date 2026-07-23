260cf34168f92c0e7931877bce1e063af01fc519
Sanjana_Kumbhar <kumbharsanjana97@gmail.com>
2026-07-24 00:52:03 +0530

    Create              ├── enrollment-form.component.ts

---
+++ Angular/4.Template-Driven Forms & Validation./src/app/pages/enrollment-form/├── enrollment-form.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-enrollment-form',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './enrollment-form.component.html',
+  styleUrls: ['./enrollment-form.component.css']
+})
+export class EnrollmentFormComponent {
+
+  studentName = '';
+  studentEmail = '';
+  courseId: number | null = null;
+  preferredSemester = 'Odd';
+  agreeToTerms = false;
+
+  submitted = false;
+
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+    console.log(form.valid);
+
+    this.submitted = true;
+
+    alert('Enrollment request submitted successfully!');
+  }
+
+}
