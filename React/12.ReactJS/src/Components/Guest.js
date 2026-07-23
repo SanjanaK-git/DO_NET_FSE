88fadd8de213caefb6681920fd6037a9a57e85da
Sanjana_Kumbhar <kumbharsanjana97@gmail.com>
2026-07-23 21:03:08 +0530

    Create Guest.js

---
+++ React/12.ReactJS/src/Components/Guest.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Guest() {
+  return (
+    <div>
+      <h2>Welcome Guest User</h2>
+
+      <h3>Available Flight Details</h3>
+
+      <ul>
+        <li>Delhi to Mumbai - ₹5000</li>
+        <li>Bhubaneswar to Bangalore - ₹6000</li>
+        <li>Hyderabad to Chennai - ₹4500</li>
+      </ul>
+
+      <p>Please login to book tickets.</p>
+    </div>
+  );
+}
+
+export default Guest;
