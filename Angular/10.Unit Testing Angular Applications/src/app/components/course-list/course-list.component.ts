0c1c842703be32309d377a78284ab936fb80efe3
Sanjana_Kumbhar <kumbharsanjana97@gmail.com>
2026-07-25 20:26:19 +0530

    Create course-list.component.ts

---
+++ Angular/10.Unit Testing Angular Applications/src/app/components/course-list/course-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+import { Course } from '../../models/course';
+import { loadCourses } from '../../store/course/course.actions';
+import { selectAllCourses } from '../../store/course/course.selectors';
+
+@Component({
+  selector: 'app-course-list',
+  templateUrl: './course-list.component.html'
+})
+export class CourseListComponent implements OnInit {
+
+  courses$: Observable<Course[]>;
+
+  constructor(private store: Store) {
+    this.courses$ = this.store.select(selectAllCourses);
+  }
+
+  ngOnInit(): void {
+    this.store.dispatch(loadCourses());
+  }
+}
