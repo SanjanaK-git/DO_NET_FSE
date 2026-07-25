24f43a32ca8fa4026f399c513be33b8ebe1980cd
Sanjana_Kumbhar <kumbharsanjana97@gmail.com>
2026-07-25 13:04:03 +0530

    Add ReactiveEnrollmentFormComponent
    Implemented a reactive enrollment form component with FormBuilder.

---
+++ Angular/student-course-portal/student-course-portal/src/app/components/reactive-enrollment-form/reactive-enrollment-form.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+
+@Component({
+selector:'app-reactive-enrollment-form',
+templateUrl:'./reactive-enrollment-form.component.html',
+styleUrls:['./reactive-enrollment-form.component.css']
+})
+export class ReactiveEnrollmentFormComponent{
+
+
+enrollForm:FormGroup;
+
+
+constructor(private fb:FormBuilder){
+
+this.enrollForm=this.fb.group({
+
+courseId:['']
+
+});
+
+}
+
+
+submit(){
+
+console.log(this.enrollForm.value);
+
+}
+
+
+}
