6679c7f155e879e3bafcb5a54ab2c7492c0f42ce
Sanjana_Kumbhar <kumbharsanjana97@gmail.com>
2026-07-25 13:16:31 +0530

    Create app.component.ts

---
+++ Angular/student-course-portal/student-course-portal/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { LoadingSpinnerComponent } from './components/loading-spinner/loading-spinner.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    LoadingSpinnerComponent
+  ],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  title = 'Student Course Portal';
+
+}
