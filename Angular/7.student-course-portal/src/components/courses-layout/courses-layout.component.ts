6f23f7d0329d256fb58fae4802560badc3095b62
Sanjana_Kumbhar <kumbharsanjana97@gmail.com>
2026-07-25 10:34:14 +0530

    Add CoursesLayoutComponent with basic structure

---
+++ Angular/7.student-course-portal/src/components/courses-layout/courses-layout.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+selector:'app-courses-layout',
+templateUrl:'./courses-layout.component.html'
+})
+export class CoursesLayoutComponent{
+
+}
